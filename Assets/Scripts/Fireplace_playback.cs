7fcba56cbb7f264520b6a15a2e69bb563303558d
Agnieszka Wójtowicz <awojtowicz4@edu.cdv.pl>
2025-12-12 18:19:01 +0100

    Działające kroki, a drzwi na wpół

--- Assets/Scripts/Fireplace_playback.cs
+++ Assets/Scripts/Fireplace_playback.cs
@@ -8,11 +8,11 @@ public class Fireplace_playback : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        fireplaceEmitter.SetParameter("Fire", 0);
+        fireplaceEmitter.SetParameter("fireplace", 0);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        fireplaceEmitter.SetParameter("Fire", 1);
+        fireplaceEmitter.SetParameter("fireplace", 1);
     }
 }
