2c93fd0904ed1dec1d274ccac8cc7a1b734e8e4d
Agnieszka Wójtowicz <awojtowicz4@edu.cdv.pl>
2025-12-12 20:31:46 +0100

    Działające drzwi i ogień na wpół

--- Assets/Scripts/Fireplace_playback.cs
+++ Assets/Scripts/Fireplace_playback.cs
@@ -8,11 +8,11 @@ public class Fireplace_playback : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        fireplaceEmitter.SetParameter("fireplace", 0);
+        fireplaceEmitter.SetParameter("Fire", 0);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        fireplaceEmitter.SetParameter("fireplace", 1);
+        fireplaceEmitter.SetParameter("Fire", 1);
     }
 }
